cb0a34dbb282fd466675e5f408cc6ed20f04c329
Octave Larose <octave.larose@hotmail.fr>
2021-06-11 12:34:34 +0100

    Access modifiers on CT generated code

--- src/main/java/com/github/octavelarose/codegenerator/builders/programs/CTParserProgramBuilder.java
+++ src/main/java/com/github/octavelarose/codegenerator/builders/programs/CTParserProgramBuilder.java
@@ -1,7 +1,10 @@
 package com.github.octavelarose.codegenerator.builders.programs;
 
+import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.type.Type;
 import com.github.javaparser.ast.type.VoidType;
 import com.github.octavelarose.codegenerator.builders.BuildFailedException;
 import com.github.octavelarose.codegenerator.builders.classes.BasicClassBuilder;
@@ -46,6 +49,7 @@ public class CTParserProgramBuilder implements ProgramBuilder {
 
     public HashMap<String, ClassBuilder> build() throws BuildFailedException {
         HashMap<String, ClassBuilder> classBuilders = new HashMap<>();
+        String PKG_NAME = "com.abc.random";
 
         // Using a hardcoded calltrace for now, should be input arg in the future
         String filename = "./input_data/calltrace_Mandelbrot.txt";
@@ -58,22 +62,64 @@ public class CTParserProgramBuilder implements ProgramBuilder {
 
             ClassBuilder classCb;
             if (!classBuilders.containsKey(className)) {
-                classCb = new BasicClassBuilder(className, 0, 0, "com.abc.random");
+                classCb = new BasicClassBuilder(className, 0, 0, PKG_NAME);
                 classBuilders.put(className, classCb);
             } else {
                 classCb = classBuilders.get(className);
             }
 
-            if(!classCb.hasMethod(methodName)) {
-                BlockStmt methodBody = new BlockStmt();
-                // TODO: Can take access modifiers into account.
-                // TODO: Can take return value/parameters into account. (a hassle, need to figure out the syntax used by DiSL, i.e "(LBenchmark;)V")
-                classCb.addMethod(methodName, new VoidType(), new NodeList<>(), methodBody, new NodeList<>());
-            }
+            if(!classCb.hasMethod(methodName))
+                this.addNewMethodToClass(methodName, methodArr, classCb);
 
 //            System.out.println(className + " " + methodName);
         }
 
         return classBuilders;
+    }
+
+    /**
+     * Adds a new method to a class, setting adequate parameters beforehand.
+     * @param methodName The name of the method.
+     * @param methodArr Info about the method in general.
+     * @param classCb The class(builder) to which it needs to be added.
+     */
+    private void addNewMethodToClass(String methodName, List<String> methodArr, ClassBuilder classCb) {
+        NodeList<Modifier> modifiers = this.getModifiersListFromScope(methodArr.get(SCOPE));
+
+        // In the future, will need to contain info ; probably just a sleep() operation at first
+        BlockStmt methodBody = new BlockStmt();
+
+        // Will need to be fetched from the descriptor.
+        // ..."Type.getArgumentTypes(desc)" could be useful, it's what the ASM lib uses internally at some point.
+        // https://asm.ow2.io/faq.html#Q7 The explanation for the syntax
+        Type returnType = new VoidType();
+
+        // Will need to be fetched from the descriptor as well.
+        NodeList<Parameter> parameters = new NodeList<>();
+
+        classCb.addMethod(methodName, returnType, parameters, methodBody, modifiers);
+    }
+
+    /**
+     * @param scope A string defining the method's scope (public, private...)
+     * @return a NodeList of Modifier objects, corresponding to the input scope
+     */
+    private NodeList<Modifier> getModifiersListFromScope(String scope) {
+        NodeList<Modifier> modifiers;
+
+        switch (scope) {
+            case "pub":
+                modifiers = new NodeList<>(Modifier.publicModifier());
+                break;
+            case "pri":
+                modifiers = new NodeList<>(Modifier.privateModifier());
+                break;
+            case "pro":
+                modifiers = new NodeList<>(Modifier.protectedModifier());
+                break;
+            default:
+                modifiers = new NodeList<>();
+        }
+        return modifiers;
     }
 }
