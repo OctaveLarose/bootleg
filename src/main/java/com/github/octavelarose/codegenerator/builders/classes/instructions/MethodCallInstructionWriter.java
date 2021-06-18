4a404a9a62f19600bf2c6e5c37b613a4e881e0d1
Octave Larose <octave.larose@hotmail.fr>
2021-06-18 12:25:18 +0100

    Accounting for method calls from same class and constructor calls. TODO: add back argument handling with dummy values

--- src/main/java/com/github/octavelarose/codegenerator/builders/classes/instructions/MethodCallInstructionWriter.java
+++ src/main/java/com/github/octavelarose/codegenerator/builders/classes/instructions/MethodCallInstructionWriter.java
@@ -5,10 +5,7 @@ import com.github.javaparser.ast.body.CallableDeclaration;
 import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.VariableDeclarator;
-import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.expr.NameExpr;
-import com.github.javaparser.ast.expr.ObjectCreationExpr;
-import com.github.javaparser.ast.expr.VariableDeclarationExpr;
+import com.github.javaparser.ast.expr.*;
 import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
@@ -31,17 +28,10 @@ public class MethodCallInstructionWriter {
     CallableDeclaration<?> callerMethod;
     CallableDeclaration<?> calleeMethod;
 
-//    public MethodCallInstructionWriter(CallableDeclaration<?> callerMethod, CallableDeclaration<?> calleeMethod) {
-//        this.callerMethod = callerMethod;
-//        this.calleeMethod = calleeMethod;
-//    }
-
     public MethodCallInstructionWriter(ClassBuilder callerClass, CallableDeclaration.Signature callerMethodSignature,
                                        ClassBuilder calleeClass, CallableDeclaration.Signature calleeMethodSignature) {
-        this.callerMethod = callerClass.getMethodFromSignature(callerMethodSignature); // TODO use Signature object
+        this.callerMethod = callerClass.getMethodFromSignature(callerMethodSignature);
         this.calleeMethod = calleeClass.getMethodFromSignature(calleeMethodSignature);
-        if (this.callerMethod == null || this.calleeMethod == null)
-            System.out.println("olala keskisspass " + callerMethodSignature.asString() + ", " + calleeMethodSignature.asString());
         this.callerClass = callerClass;
         this.calleeClass = calleeClass;
     }
@@ -65,31 +55,49 @@ public class MethodCallInstructionWriter {
             throw new BuildFailedException("Method is neither a classic method nor a constructor");
 
         String methodCallStatementStr = getMethodSignatureWithValues(calleeMethod.getSignature().asString());
-//        System.out.println(methodCallStatementStr);
-//        methodBody.addStatement(methodCallStatementStr);
 
-        // TODO make this a method call to a class since it's duplicated a few times here and there
+        // TODO make this a method call to a class since it's duplicated a few times here and there in my codebase
         Optional<ClassOrInterfaceType> classWithName = new JavaParser()
-                .parseClassOrInterfaceType(callerClass.getName())
+                .parseClassOrInterfaceType(calleeClass.getName())
                 .getResult();
 
         if (classWithName.isEmpty())
             throw new BuildFailedException("Couldn't parse class " + classWithName);
 
-        if (!isClassInstantiationInMethod(methodBody, callerClass.getName())) {
-            // TODO import statement
-            methodBody.addStatement(0, new VariableDeclarationExpr(
-                    new VariableDeclarator(classWithName.get(), callerClass.getName().toLowerCase(),
-                            new ObjectCreationExpr().setType(classWithName.get())))
-            );
-        }
-
         // TODO argument handling, also for the constructor call.
-        methodBody.addStatement(
-                new MethodCallExpr(
-                        new NameExpr(callerClass.getName().toLowerCase()),
-                        calleeMethod.getName())
-        );
+        if (callerClass == calleeClass) {
+            methodBody.addStatement(
+                    new MethodCallExpr(
+                            new ThisExpr(),
+                            calleeMethod.getName())
+            );
+        } else {
+//            System.out.println(callerClass.getName() + ":" + callerMethod.getName() + ", "
+//                    + calleeClass.getName() + ":" + calleeMethod.getName());
+
+            if (calleeMethod instanceof ConstructorDeclaration) {
+                // TODO add import statement else it won't run
+                methodBody.addStatement(0, new VariableDeclarationExpr(
+                        new VariableDeclarator(classWithName.get(), calleeClass.getName().toLowerCase(),
+                                new ObjectCreationExpr().setType(classWithName.get())))
+                );
+            } else {
+                methodBody.addStatement(
+                        new MethodCallExpr(
+                                new NameExpr(calleeClass.getName().toLowerCase()),
+                                calleeMethod.getName())
+                );
+            }
+
+            // Ugly safeguard. If an object is returned from a method call, or if it's in a field, it can't be detected
+            // So for now I'll just create a new instance of it in every method that needs it. TODO improve, but how?
+            if (!isClassInstantiationInMethod(methodBody, calleeClass.getName())) {
+                methodBody.addStatement(0, new VariableDeclarationExpr(
+                        new VariableDeclarator(classWithName.get(), calleeClass.getName().toLowerCase(),
+                                new ObjectCreationExpr().setType(classWithName.get())))
+                );
+            }
+        }
     }
 
     /**
