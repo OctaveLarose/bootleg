e3640cdb0272e5f624e838e58adf07918299690c
Octave Larose <octave.larose@hotmail.fr>
2021-07-27 11:41:19 +0100

    Updated bash script to run with operations files

--- build_run_all_awfy.sh
+++ build_run_all_awfy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-AWFY_FILES="./input_data/disl_awfy_calltraces/*"
+AWFY_CT_DIR="./input_data/disl_awfy_calltraces"
+AWFY_OP_DIR="./input_data/disl_awfy_operations"
 NBR_FAILS=0
 
-for FILE in $AWFY_FILES; 
+for FILE in $AWFY_CT_DIR/*.txt;
 do
     echo "Running with CT file: $FILE"
-    ./gradlew run --args="--ct-file $FILE"
+    CT_NAME=`echo $FILE | sed -e 's/.*calltrace_\(.*\)\.txt/\1/'`
+    ./gradlew run --args="--ct-file $FILE --op-file $AWFY_OP_DIR/operations_$CT_NAME.txt"
     if [ $? -eq 1 ]; then ((NBR_FAILS++)); fi    
 done
 
-echo "---$NBR_FAILS total number of build/run fails (out of `ls $AWFY_FILES | wc -l` files)---"
+echo "---$NBR_FAILS total number of build/run fails (out of `ls $AWFY_CT_DIR | wc -l` files)---"
